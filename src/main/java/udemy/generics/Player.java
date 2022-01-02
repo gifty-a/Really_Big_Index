68ea186030692d7b16f3e30ac1c9e62a2807e9e0
gifty-a <ms.giftyakonnor@gmail.com>
2022-01-02 15:30:50 +0000

    Added more fields to the Player class

--- src/main/java/udemy/generics/Player.java
+++ src/main/java/udemy/generics/Player.java
@@ -4,12 +4,96 @@ public class Player {
     private String playerNm;
     private int playerAge;
     private String countryOfOrigin;
+    double rating;
+    int goals, assists, red, yellow;
 
     public Player(String playerNm, int playerAge, String countryOfOrigin) {
         this.playerNm = playerNm;
         this.playerAge = playerAge;
         this.countryOfOrigin = countryOfOrigin;
+        this.rating = 0.0;
+        this.goals= this.yellow = this.assists = this.red = 0;
     }
 
+    public Player(String playerNm, int playerAge, String countryOfOrigin, double rating, int goals, int assists, int red, int yellow) {
+        this(playerNm,playerAge,countryOfOrigin);
+        this.rating = rating;
+        this.goals = goals;
+        this.assists = assists;
+        this.red = red;
+        this.yellow = yellow;
+    }
 
+    public String getPlayerNm() {
+        return playerNm;
+    }
+
+    public void setPlayerNm(String playerNm) {
+        this.playerNm = playerNm;
+    }
+
+    public int getPlayerAge() {
+        return playerAge;
+    }
+
+    public void setPlayerAge(int playerAge) {
+        this.playerAge = playerAge;
+    }
+
+    public String getCountryOfOrigin() {
+        return countryOfOrigin;
+    }
+
+    public void setCountryOfOrigin(String countryOfOrigin) {
+        this.countryOfOrigin = countryOfOrigin;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    public int getYellow() {
+        return yellow;
+    }
+
+    public void setYellow(int yellow) {
+        this.yellow = yellow;
+    }
+
+    public String printPlayerStats() {
+        return "Goals : " + goals +"\n" +
+                "Assists : " + assists + "\n" +
+                "Yellow : " + yellow + "\n" +
+                "Red : " + red + "\n" +
+                "Rating : " + rating + "\n" +
+                "---------------------------";
+    }
 }
--- src/main/java/udemy/generics/Team.java
+++ src/main/java/udemy/generics/Team.java
@@ -1,8 +1,107 @@
 package main.java.udemy.generics;
 
+import java.util.ArrayList;
+
 public class Team<T extends Player> implements Comparable<Team<T>> {
+    private String teamNm;
+    int gamesPlayed;
+    int gamesWon;
+    int gamesLost;
+    int gamesTied;
+    ArrayList<T> members;
+
+    public Team(String teamNm) {
+        this.teamNm = teamNm;
+        this.gamesPlayed = 0;
+        this.gamesWon = 0;
+        this.gamesLost = 0;
+        this.gamesTied = 0;
+        this.members = new ArrayList<>();
+    }
+
+    public String getTeamNm() {
+        return teamNm;
+    }
+
+    public void setTeamNm(String teamNm) {
+        this.teamNm = teamNm;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed(int gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+
+    public void setGamesWon(int gamesWon) {
+        this.gamesWon = gamesWon;
+    }
+
+    public int getGamesLost() {
+        return gamesLost;
+    }
+
+    public void setGamesLost(int gamesLost) {
+        this.gamesLost = gamesLost;
+    }
+
+    public int getGamesTied() {
+        return gamesTied;
+    }
+
+    public void setGamesTied(int gamesTied) {
+        this.gamesTied = gamesTied;
+    }
+
+    public ArrayList<T> getMembers() {
+        return members;
+    }
+
+    public boolean addTeamMate(T teamMate) {
+        if (this.members.contains(teamMate)) {
+            System.out.println(teamMate.getPlayerNm() + "Already plays for this team");
+            return false;
+        } else {
+            this.members.add(teamMate);
+            teamMate.printPlayerStats();
+            return true;
+        }
+    }
+
+    public int getNumOfPlayers() {
+        return this.members.size();
+    }
+
+    public void matchResult(Team<T> opponent, int ourScore, int opponentScore) {
+        String message;
+        if (ourScore > opponentScore) {
+            gamesWon++;
+            message = " beat ";
+        } else if (ourScore < opponentScore) {
+            gamesLost++;
+            message = " lost to ";
+        } else {
+            gamesTied++;
+            message = " drew with ";
+        }
+        gamesPlayed++;
+        System.out.println(this.getTeamNm() + message + opponent.getTeamNm());
+    }
+
+    public int ranking() {
+        return (gamesWon * 2) + gamesTied;
+    }
+
     @Override
-    public int compareTo(Team<T> o) {
-        return 0;
+    public int compareTo(Team<T> team) {
+        if (this.ranking() > team.ranking()) return -1;
+        else if (this.ranking() < team.ranking()) return 1;
+        else return 0;
     }
 }
