f8bd727bcdd2809d935970a48c41b8b68178417f
gifty-a <ms.giftyakonnor@gmail.com>
2022-01-15 08:45:20 +0000

    Wildcard use in generics

--- src/main/java/bo0k/generics/BoundedWildCardUseApp.java
+++ src/main/java/bo0k/generics/BoundedWildCardUseApp.java
@@ -3,13 +3,14 @@ package main.java.bo0k.generics;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BoundedWildCardUseClass {
+public class BoundedWildCardUseApp {
     public static void main(String[] args) {
         List<Integer> integerList = new ArrayList<>();
         integerList.add(12);
         integerList.add(99);
         integerList.add(1990);
         double integerListSum = BoundedWildCardUse.sum(integerList);
+        System.out.println("Integer List is " + integerList);
         System.out.println("Sum of Integer List is " + integerListSum);
     }
 }
